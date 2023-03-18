51ae02332ebcbd6006492f7be91f8cb87b618b44
Thibault-GILLARD <thibault.gillard@epfedu.fr>
2023-03-18 23:13:09 +0100

    Add functionality to parse CSV file and send student data as JSON

--- app.js
+++ app.js
@@ -2,24 +2,6 @@ const express = require('express')
 const app = express()
 const port = 3000
 const fs = require("fs");
-/*
-
-
-app.get("/students-csv", (req, res) => {
-  // example based on a CSV file
-  fs.readFile("./students.csv", "utf8", (err, data) => {
-    res.send(data);
-  });
-});
-
-app.get("/students", (req, res) => {
-  // basic example: we send hard-coded data
-  res.send([
-    { name: "Eric Burel", school: "LBKE" },
-    { name: "Harry Potter", school: "Poudlard" },
-  ]);
-});
-*/
 
 // basic data to send to the client 
 const student_data = [
@@ -52,6 +34,29 @@ app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
 
+// Bonus: how to parse a CSV file
+app.get("/csv_students_bonus", (req, res) => {
+  // rowSeparator is the character that separates rows
+  const rowSeparator = "\n";
+  // cellSeparator is the character that separates cells
+  const cellSeparator = ",";
+  fs.readFile("./students.csv", "utf8", (err, data) => {
+    // split the data into rows
+    const rows = data.split(rowSeparator);
+    // first row is an header I isolate it
+    const [headerRow, ...contentRows] = rows;
+    const header = headerRow.split(cellSeparator);
+    const students = contentRows.map((row) => {
+      const cells = row.split(cellSeparator);
+      const student = {
+        [header[0]]: cells[0],
+        [header[1]]: cells[1],
+      };
+      return student;
+    });
+    res.send(students);
+  });
+});
 
 
 
